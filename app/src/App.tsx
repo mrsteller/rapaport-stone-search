34dfec8756c1e1561adbc181631a303d14d99b76
Miriam Teller <miriam.teller@benmanage.com>
2021-06-15 21:19:55 +0300

    add more state to control filters

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Container, Typography, Box } from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import { Search } from "./search";
 
-function App() {
-  return (
-    <Container maxWidth="md">
-      <Box textAlign="center">
-        <Typography variant="h1">Precious Stones</Typography>
-      </Box>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: { padding: "4em 1em" },
+  })
+);
 
-      <Search />
-    </Container>
+function App() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Container maxWidth="md" className={classes.root}>
+        <Box textAlign="center" p={4}>
+          <Typography variant="h3">Rapaport Stone Listings</Typography>
+        </Box>
+        <Search />
+      </Container>
+    </div>
   );
 }
 
--- app/src/search.tsx
+++ app/src/search.tsx
@@ -4,6 +4,48 @@ import { Container, Box, TextField } from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 import stoneList from "./stones.json";
 
+const shapeList = [
+  "Round",
+  "‌Princess",
+  "‌Emerald",
+  "‌Asscher",
+  "‌Radiant",
+  "‌Square‌",
+  "‌Radiant",
+  "‌Pear",
+  "‌Oval‌",
+];
+const colorList = [
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+];
+
+const clarityList = [
+  "FL",
+  "IF",
+  "VVS1",
+  "VVS2",
+  "VS1",
+  "VS2",
+  "SI1",
+  "SI2",
+  "SI3",
+  "I1",
+  "I2",
+  "I3",
+];
+
 type SearchProps = {};
 
 type Stone = {
@@ -16,13 +58,32 @@ type Stone = {
 
 export const Search = (props: SearchProps) => {
   const [stones, setStones] = useState<Array<Stone>>([]);
+  const [search, setSearch] = useState<string>("");
+
+  const [shapes, setShapes] = useState<Array<string>>(shapeList);
+  const [color, setColor] = useState<Array<string>>(colorList);
+  const [clarity, setClarity] = useState<Array<string>>(clarityList);
 
   useEffect(() => {
     setStones(stoneList);
   }, []);
 
+  useEffect(() => {
+    const filterShape = stoneList.filter((s) => s.shape.includes(search));
+    const filterColor = stoneList.filter((s) => s.color?.includes(search));
+    const filterClarity = stoneList.filter((s) => s.clarity.includes(search));
+
+    setStones(filterShape.concat(filterColor).concat(filterClarity));
+  }, [search]);
+
   const getLabel = (stone: Stone) => {
-    return `${stone.shape} in ${stone.type}`;
+    const colorLabel = `Color: ${stone.color} in ${stone.type}`;
+    const shapeLabel = `Shape: ${stone.shape} in ${stone.type}`;
+    const clarityLabel = `Clarity: ${stone.clarity} in ${stone.type}`;
+
+    return `${stone.shape} in ${stone.type} ${stone.clarity} ${
+      stone.color ?? ""
+    }`;
   };
 
   return (
@@ -32,6 +93,8 @@ export const Search = (props: SearchProps) => {
           <Autocomplete
             id="stone-search"
             fullWidth
+            inputValue={search}
+            onInputChange={(e, value) => setSearch(value)}
             options={stones}
             getOptionLabel={getLabel}
             renderInput={(params) => (
