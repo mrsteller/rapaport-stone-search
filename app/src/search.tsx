f8ab8ad59cdad6e683e0dd25ceeacc419aaefec1
Miriam Teller <miriam.teller@benmanage.com>
2021-06-19 22:50:09 +0300

    hmm where's the other shapes??

--- app/src/search.tsx
+++ app/src/search.tsx
@@ -8,9 +8,6 @@ import {
   //  CardMedia,
   Container,
   Grid,
-  Menu,
-  MenuItem,
-  IconButton,
   TextField,
   Tooltip,
   Typography,
@@ -18,7 +15,6 @@ import {
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import stoneData from "./stones.json";
 import { Stone, stoneTypes, shape, clarity, color } from "./models";
-import { ArrowDropDown, Close } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -45,11 +41,7 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-//TODO: options as objects with labels, value, type etc
-//open menu on type not click, check input still works
 //styles - fixed bar and header
-//filter options
-//configure click option
 type Option = { label: string; type: string; value: string };
 
 export const Search = () => {
@@ -58,16 +50,6 @@ export const Search = () => {
   const [search, setSearch] = useState<string>("");
   const [options, setOptions] = useState<Array<any>>([]);
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
-
-  // useEffect(() => {
-  //   console.log("selected", selectedOption);
-  //   if (selectedOption !== null) {
-  //     const filteredStones = stones.filter(
-  //       (s) => s.type === selectedOption.type
-  //     );
-  //     setStones(filteredStones);
-  //   }
-  // }, [selectedOption, stones]);
 
   useEffect(() => {
     setStones(stoneData);
@@ -95,89 +77,22 @@ export const Search = () => {
     setOptions(allShapes.concat(allColors).concat(allClarity));
   };
 
-  // useEffect(() => {
-  //   const filterShape = stoneData.filter((s) =>
-  //     s.shape.toLocaleLowerCase().startsWith(search.toLocaleLowerCase())
-  //   );
-  //   const filterColor = stoneData.filter((s) =>
-  //     s.color?.toLocaleLowerCase().startsWith(search.toLocaleLowerCase())
-  //   );
-  //   const filterClarity = stoneData.filter((s) =>
-  //     s.clarity.toLocaleLowerCase().startsWith(search.toLocaleLowerCase())
-  //   );
-  //   setStones(filterShape.concat(filterColor).concat(filterClarity));
-
-  //   if (search !== "" && options !== null) {
-  //     const filterOptions = options.filter((o) =>
-  //       o.value.toLocaleLowerCase().startsWith(search.toLocaleLowerCase())
-  //     );
-  //     setOptions(filterOptions);
-  //   } else {
-  //     initialiseOptions();
-  //   }
-  // }, [search, options]);
-
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const openMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const closeMenu = () => {
-    setAnchorEl(null);
-  };
+  useEffect(() => {
+    console.log(selectedOption);
+    if (selectedOption) {
+      const filteredStones = stoneData.filter(
+        (s) => s.type === selectedOption.type
+      );
+      setStones(filteredStones);
+    } else {
+      setStones(stoneData);
+    }
+  }, [selectedOption]);
 
   return (
     <div>
       <Container>
         <Box>
-          {/* <TextField
-            className={classes.searchBar} //display:fixed
-            variant="outlined"
-            name="search"
-            label="Search"
-            fullWidth
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            InputProps={{
-              endAdornment: (
-                <>
-                  {search !== "" && (
-                    <IconButton onClick={() => setSearch("")}>
-                      <Close />
-                    </IconButton>
-                  )}
-                  <IconButton onClick={openMenu}>
-                    <ArrowDropDown />
-                  </IconButton>
-                </>
-              ),
-            }}
-          />
-          <Menu
-            className={classes.dropdown}
-            id="search-dropdown"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={closeMenu}
-            getContentAnchorEl={null}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            PaperProps={{
-              className: classes.dropdown,
-            }}
-          >
-            {options.map((o, i) => (
-              <MenuItem key={i} button onClick={() => setSelectedOption(o)}>
-                {o.label}
-              </MenuItem>
-            ))}
-          </Menu> */}
           <Autocomplete
             filterOptions={createFilterOptions({
               matchFrom: "start",
